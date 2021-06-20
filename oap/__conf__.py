b1e2e15118d5b73edf5d1848ccbc207921ecbf91
lcsgrlch <lucasgrulich@gmx.de>
2021-06-20 17:36:10 +0200

    Alpha Version 0.0.14

--- oap/__conf__.py
+++ oap/__conf__.py
@@ -1,5 +1,5 @@
 __author__ = "Lucas Grulich (grulich@uni-mainz.de)"
-__version__ = "0.0.13"
+__version__ = "0.0.14"
 
 # --- Globals ----------------------------------------------------------------------------------------------------------
 MONOSCALE_SHADOWLEVEL = 1
